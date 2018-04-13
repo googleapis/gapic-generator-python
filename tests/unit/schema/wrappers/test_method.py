2d6bfb0efd2570b41a3494e07cf56469e12038fa
Luke Sneeringer <lukesneeringer@google.com>
2018-04-13 14:17:08 -0700

    Add unit tests for method.

--- tests/unit/schema/wrappers/test_method.py
+++ tests/unit/schema/wrappers/test_method.py
@@ -16,11 +16,68 @@ from google.protobuf import descriptor_pb2
 
 from api_factory.schema import metadata
 from api_factory.schema import wrappers
+from api_factory.schema.pb import overload_pb2
 
 
 def get_method() -> wrappers.Method:
-    method_pb2 = descriptor_pb2.MethodDescriptorProto(
+    Location = descriptor_pb2.SourceCodeInfo.Location
+
+    # Create the address where this method lives, and the types live,
+    # and make them distinct.
+    method_addr = metadata.Address(package=['foo', 'bar'], module='baz')
+    types_addr = metadata.Address(package=['foo', 'bar'], module='bacon')
+
+    # Create the method pb2 and set an overload in it.
+    method_pb = descriptor_pb2.MethodDescriptorProto(
         name='DoTheThings',
         input_type='foo.bar.Input',
         output_type='foo.bar.Output',
     )
+
+    # Set an overload in the method descriptor.
+    ext_key = overload_pb2.overloads
+    method_pb.options.Extensions[ext_key].extend([overload_pb2.Overload()])
+
+    # Instantiate the wrapper class.
+    return wrappers.Method(
+        method_pb=method_pb,
+        input=wrappers.MessageType(
+            fields=[],
+            message_pb=descriptor_pb2.DescriptorProto(name='Input'),
+            meta=metadata.Metadata(
+                address=types_addr,
+                documentation=Location(),
+            ),
+        ),
+        output=wrappers.MessageType(
+            fields=[],
+            message_pb=descriptor_pb2.DescriptorProto(name='Output'),
+            meta=metadata.Metadata(
+                address=types_addr,
+                documentation=Location(),
+            ),
+        ),
+        meta=metadata.Metadata(
+            address=method_addr,
+            documentation=Location(),
+        ),
+    )
+
+
+def test_method_properties():
+    method = get_method()
+    assert method.name == 'DoTheThings'
+
+
+def test_method_types():
+    method = get_method()
+    assert method.input.name == 'Input'
+    assert method.input.pb2_module == 'bacon_pb2'
+    assert method.output.name == 'Output'
+    assert method.output.pb2_module == 'bacon_pb2'
+
+
+def test_method_overloads():
+    method = get_method()
+    for overload in method.overloads:
+        assert isinstance(overload, overload_pb2.Overload)
