e6dedbf6aa0937a792aa4c7b5d8b3559b2aee71d
Luke Sneeringer <lukesneeringer@google.com>
2018-04-19 11:26:49 -0700

    Output appropriate client library namespace.
    This commit makes it such that library code is output into an
    appropriate import structure.
    
    Prior to this, each service was written to the output directory
    at the top level. Now, the API's namespace and name are used.
    
    Therefore, for an API named "Foo" with version "v1", which has
    `service Bar`...
    
        Before: import bar
        Now: from foo_v1 import bar
    
    And if the namespace is ("Spam", "Eggs")...
    
        Before: import bar
        Now: from spam.eggs.foo_v1 import bar
    
    Additionally, the API's name is directly importable, with and
    without the version:
    
       Now: from spam.eggs import foo
       Now: from spam.eggs import foo_v1
    
    These are actually the preferred import approach.

--- api_factory/schema/api.py
+++ api_factory/schema/api.py
@@ -61,6 +61,26 @@ class API:
         return ' '.join(list(self.client.namespace) + [self.client.name])
 
     @property
+    def module_name(self) -> str:
+        """Return the appropriate Python module name."""
+        return self.client.name.lower()
+
+    @property
+    def version(self) -> str:
+        """Return the appropriate API version."""
+        return self.client.version
+
+    @property
+    def versioned_module_name(self) -> str:
+        """Return the versiond module name (e.g. ``apiname_v1``).
+
+        If there is no version, this is the same as ``module_name``.
+        """
+        if self.version:
+            return f'{self.module_name}_{self.version}'
+        return self.module_name
+
+    @property
     def warehouse_package_name(self) -> str:
         """Return the appropriate Python package name for Warehouse."""
         # Sanity check: If no name is provided, use a clearly placeholder
