470d5ec5f8a062307fbafa75c37835f8c4d74313
Luke Sneeringer <lukesneeringer@google.com>
2018-07-10 08:06:50 -0700

    The client works again!

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ with io.open(os.path.join(PACKAGE_ROOT, 'README.rst')) as file_obj:
 
 setup(
     name='gapic-generator',
-    version='0.0.3',
+    version='0.0.4',
     license='Apache 2.0',
     author='Luke Sneeringer',
     author_email='lukesneeringer@google.com',
@@ -42,7 +42,7 @@ setup(
     include_package_data=True,
     install_requires=(
         'click >= 6.7',
-        'googleapis-common-protos >= 1.6.0b3',
+        'googleapis-common-protos >= 1.6.0b4',
         'grpcio >= 1.9.1',
         'jinja2 >= 2.10',
         'protobuf >= 3.5.1',
