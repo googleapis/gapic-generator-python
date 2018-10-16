a1b11ab52da7c84393d1b9d0c35416cdeb5a7f37
Luke Sneeringer <lukesneeringer@google.com>
2018-10-16 15:09:58 -0700

    Remove unused import.

--- tests/unit/schema/test_metadata.py
+++ tests/unit/schema/test_metadata.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import collections
 import typing
 
 from google.protobuf import descriptor_pb2
