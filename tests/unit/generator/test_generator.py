6fc58548031aecbc83bbb56d5966631540384361
Dov Shlachter <dovs@google.com>
2019-08-12 10:18:52 -0700

    Clean up unused and spurious imports

--- tests/unit/generator/test_generator.py
+++ tests/unit/generator/test_generator.py
@@ -12,12 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import pdb
 from textwrap import dedent
 from typing import Mapping
 from unittest import mock
 
-import itertools
 import jinja2
 import pytest
 
