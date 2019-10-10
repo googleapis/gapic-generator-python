546ae4fe3492af39c5f0a253f17b33831d09b4d7
Dov Shlachter <dovs@google.com>
2019-10-10 10:10:41 -0700

    Add test and impl for request messages with primitive fields
    Fields that are strings, bools, ints, or floats are permitted.
    
    Mismatch between the type of the assigned value and type of the field
    is NOT handled by samplegen yet.

--- gapic/samplegen/samplegen.py
+++ gapic/samplegen/samplegen.py
@@ -280,14 +280,17 @@ class Validator:
                     # and requires less munging of the assigned value
                     duplicate["value"] = f"'{val}'"
                     break
+                elif attr.is_primitive:
+                    # Only valid if this is the last attribute in the chain.
+                    break
                 else:
                     raise TypeError
 
             if i != len(attr_chain) - 1:
-                # We broke out of the loop after processing an enum.
+                # We broke out of the loop after processing an enum or a primitive.
                 extra_attrs = ".".join(attr_chain[i:])
                 raise types.InvalidEnumVariant(
-                    f"Attempted to reference attributes of enum value: '{extra_attrs}'")
+                    f"Attempted to reference attributes of enum value or primitive type: '{extra_attrs}'")
 
             if len(attr_chain) > 1:
                 duplicate["field"] = ".".join(attr_chain[1:])
