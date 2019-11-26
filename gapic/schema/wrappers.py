a55077b5a47de715b402bb408d9dcaad4fe2699a
Dov Shlachter <dovs@google.com>
2019-11-26 12:09:10 -0800

    Take three

--- gapic/schema/wrappers.py
+++ gapic/schema/wrappers.py
@@ -132,7 +132,8 @@ class Field:
             bool: Whether this field is repeated.
         """
         return self.label == \
-            descriptor_pb2.FieldDescriptorProto.Label.Value('LABEL_REPEATED')   # type: ignore
+            descriptor_pb2.FieldDescriptorProto.Label.Value(
+                'LABEL_REPEATED')   # type: ignore
 
     @property
     def required(self) -> bool:
