b1e7f25c8a2eb108bff5ef5d601d8f7fcc845533
Anthonios Partheniou <partheniou@google.com>
2022-08-11 01:52:04 +0000

    fix tests

--- tests/integration/goldens/logging/google/cloud/logging_v2/__init__.py
+++ tests/integration/goldens/logging/google/cloud/logging_v2/__init__.py
@@ -147,7 +147,7 @@ try:
     import pkg_resources
 
     pkg_resources.declare_namespace(__name__)
-except ImportError:
+except ImportError: # pragma: NO COVER
     import pkgutil
 
     __path__ = pkgutil.extend_path(__path__, __name__)  # type: ignore
