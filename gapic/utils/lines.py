c4349ff797a1578c0159e00d9f305a028e3bbe9c
Anthonios Partheniou <partheniou@google.com>
2023-07-11 17:57:07 +0000

    docs: preserve new line for lists

--- gapic/utils/lines.py
+++ gapic/utils/lines.py
@@ -93,11 +93,14 @@ def wrap(text: str, width: int, *, offset: Optional[int] = None, indent: int = 0
                                 break_on_hyphens=False,
                                 )
         # Strip the first \n from the text so it is not misidentified as an
-        # intentionally short line below, except when the text contains `:`
-        # as the new line is required for lists.
+        # intentionally short line below, except when the text contains a list
+        # as the new line is required for lists. Look for either `:` in the
+        # initial text or `-` in the remaining text which indicates that a list
+        # is present.
         if '\n' in text:
             initial_text = text.split('\n')[0]
-            if ":" not in initial_text:
+            reamining_text = "".join(text.split('\n')[1:])
+            if ":" not in initial_text and not reamining_text.strip().startswith('-'):
                 text = text.replace('\n', ' ', 1)
 
         # Save the new `first` line.
@@ -107,7 +110,7 @@ def wrap(text: str, width: int, *, offset: Optional[int] = None, indent: int = 0
     # the sphinx docs build will fail.
     text = re.sub(r':\n([^\n])', r':\n\n\1', text)
 
-    text = text[len(first):].strip()
+    text = text[len(first):]
     if not text:
         return first.strip()
 
