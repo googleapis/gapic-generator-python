b0af46bab17e34ea8e8b1b766b11a6b2b2b943ce
ohmayr <omairnaveed@ymail.com>
2023-11-29 16:31:50 +0000

    revert tests

--- tests/integration/goldens/logging/tests/unit/gapic/logging_v2/test_logging_service_v2.py
+++ tests/integration/goldens/logging/tests/unit/gapic/logging_v2/test_logging_service_v2.py
@@ -357,10 +357,11 @@ def test_logging_service_v2_client_mtls_env_auto(client_class, transport_class, 
 @mock.patch.object(LoggingServiceV2AsyncClient, "DEFAULT_ENDPOINT", modify_default_endpoint(LoggingServiceV2AsyncClient))
 @mock.patch.object(LoggingServiceV2AsyncClient, "credentials", ga_credentials.AnonymousCredentials())
 def test_logging_service_v2_client_get_mtls_endpoint_and_cert_source(client_class):
-    mock_client_cert_source = client_cert_source_callback
+    mock_client_cert_source = mock.Mock()
 
     # Test the case GOOGLE_API_USE_CLIENT_CERTIFICATE is "true".
     with mock.patch.dict(os.environ, {"GOOGLE_API_USE_CLIENT_CERTIFICATE": "true"}):
+        mock_client_cert_source = mock.Mock()
         mock_api_endpoint = "foo"
         options = client_options.ClientOptions(client_cert_source=mock_client_cert_source, api_endpoint=mock_api_endpoint)
         api_endpoint, cert_source = client_class.get_mtls_endpoint_and_cert_source(options)
--- tests/integration/goldens/logging/tests/unit/gapic/logging_v2/test_metrics_service_v2.py
+++ tests/integration/goldens/logging/tests/unit/gapic/logging_v2/test_metrics_service_v2.py
@@ -355,10 +355,11 @@ def test_metrics_service_v2_client_mtls_env_auto(client_class, transport_class, 
 @mock.patch.object(MetricsServiceV2AsyncClient, "DEFAULT_ENDPOINT", modify_default_endpoint(MetricsServiceV2AsyncClient))
 @mock.patch.object(MetricsServiceV2AsyncClient, "credentials", ga_credentials.AnonymousCredentials())
 def test_metrics_service_v2_client_get_mtls_endpoint_and_cert_source(client_class):
-    mock_client_cert_source = client_cert_source_callback
+    mock_client_cert_source = mock.Mock()
 
     # Test the case GOOGLE_API_USE_CLIENT_CERTIFICATE is "true".
     with mock.patch.dict(os.environ, {"GOOGLE_API_USE_CLIENT_CERTIFICATE": "true"}):
+        mock_client_cert_source = mock.Mock()
         mock_api_endpoint = "foo"
         options = client_options.ClientOptions(client_cert_source=mock_client_cert_source, api_endpoint=mock_api_endpoint)
         api_endpoint, cert_source = client_class.get_mtls_endpoint_and_cert_source(options)
