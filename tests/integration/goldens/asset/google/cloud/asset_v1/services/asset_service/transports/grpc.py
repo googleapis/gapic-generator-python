1b0f243995990620a276ac7dbbe0ffc4db6029a3
ohmayr <omairnaveed@ymail.com>
2024-01-10 17:48:24 +0000

    only ignore credentials if credentials are not passed with channel

--- tests/integration/goldens/asset/google/cloud/asset_v1/services/asset_service/transports/grpc.py
+++ tests/integration/goldens/asset/google/cloud/asset_v1/services/asset_service/transports/grpc.py
@@ -119,6 +119,8 @@ class AssetServiceGrpcTransport(AssetServiceTransport):
             warnings.warn("client_cert_source is deprecated", DeprecationWarning)
 
         if channel:
+            # Ignore credentials if a channel was passed without credentials.
+            credentials = False if not credentials else credentials
             # If a channel was explicitly provided, set it.
             self._grpc_channel = channel
             self._ssl_channel_credentials = None
