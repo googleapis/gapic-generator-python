7c1a586eeecf526f61b09a0608fd4eacce557345
ohmayr <omairnaveed@ymail.com>
2024-01-11 09:58:15 +0000

    fix whitespace

--- tests/system/test_universe_domain.py
+++ tests/system/test_universe_domain.py
@@ -1,6 +1,6 @@
 import pytest
 
-#Note(TODO): Uncomment these test cases once showcase library is updated
+# Note(TODO): Uncomment these test cases once showcase library is updated
 # def test_universe_domain_validation_pass(echo_with_localhost_universe):
 #     assert echo_with_localhost_universe.universe_domain == "localhost:7469"
 #     assert echo_with_localhost_universe.api_endpoint == "localhost:7469"
