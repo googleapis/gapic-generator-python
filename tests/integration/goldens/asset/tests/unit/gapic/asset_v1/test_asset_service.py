03cf7d4f6d4d514522b4c2c01a63b2ec590c67e6
ohmayr <omairnaveed@ymail.com>
2024-01-17 21:58:41 +0000

    use client options object in instead of mock in test

--- tests/integration/goldens/asset/tests/unit/gapic/asset_v1/test_asset_service.py
+++ tests/integration/goldens/asset/tests/unit/gapic/asset_v1/test_asset_service.py
@@ -591,7 +591,7 @@ def test_asset_service_client_client_api_endpoint(client_class):
 
     # If ClientOptions does not have a universe domain attribute and GOOGLE_API_USE_MTLS_ENDPOINT="never",
     # use the DEFAULT_ENDPOINT_TEMPLATE populated with GDU as the api endpoint.
-    options = mock.Mock()
+    options = client_options.Client_Options()
     delattr(options, "universe_domain")
     with mock.patch.dict(os.environ, {"GOOGLE_API_USE_MTLS_ENDPOINT": "never"}):
         with pytest.raises(AttributeError, match="Attribute universe_domain does not exist in self._client_options. Setting universe_domain_opt to None."):
