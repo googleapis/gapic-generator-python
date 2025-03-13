89d733b2581d393f70903c5c73b99800e1be2843
Anthonios Partheniou <partheniou@google.com>
2025-03-13 23:51:24 +0000

    tests: add more tests for lro

--- tests/system/test_lro.py
+++ tests/system/test_lro.py
@@ -17,9 +17,11 @@ import pytest
 from datetime import datetime, timedelta, timezone
 
 from google import showcase
+from google.api_core import exceptions
+from google.rpc import code_pb2
 
 
-def test_lro(echo):
+def test_lro_success(echo):
     future = echo.wait(
         {
             "end_time": datetime.now(tz=timezone.utc) + timedelta(seconds=1),
@@ -33,10 +35,40 @@ def test_lro(echo):
     assert response.content.endswith("the snails...eventually.")
 
 
+def test_lro_error(echo):
+    with pytest.raises(
+        exceptions.GoogleAPICallError,
+        match="(?i)This took longer than you said it should",
+    ):
+        future = echo.wait(
+            {
+                "end_time": datetime.now(tz=timezone.utc) + timedelta(seconds=1),
+                "error": {
+                    "code": code_pb2.Code.Value("DEADLINE_EXCEEDED"),
+                    "message": "This took longer than you said it should.",
+                },
+            }
+        )
+        future.result()
+
+
+def test_lro_no_result(echo):
+    # As per the link below, `Some services might not provide a result`.
+    # Neither `error` or `response`.
+    # See https://github.com/googleapis/googleapis/blob/a6c9ed2d33105cb3dc9a0867a0a5d761b049b932/google/longrunning/operations.proto#L141
+    future = echo.wait(
+        {
+            "end_time": datetime.now(tz=timezone.utc) + timedelta(seconds=1),
+        }
+    )
+    response = future.result()
+    assert response is None
+
+
 if os.environ.get("GAPIC_PYTHON_ASYNC", "true") == "true":
 
     @pytest.mark.asyncio
-    async def test_lro_async(async_echo):
+    async def test_lro_async_success(async_echo):
 
         future = await async_echo.wait(
             {
@@ -49,3 +81,34 @@ if os.environ.get("GAPIC_PYTHON_ASYNC", "true") == "true":
         response = await future.result()
         assert isinstance(response, showcase.WaitResponse)
         assert response.content.endswith("the snails...eventually.")
+
+    @pytest.mark.asyncio
+    async def test_lro_async_error(async_echo):
+
+        with pytest.raises(
+            exceptions.GoogleAPICallError,
+            match="(?i)This took longer than you said it should",
+        ):
+            future = await async_echo.wait(
+                {
+                    "end_time": datetime.now(tz=timezone.utc) + timedelta(seconds=1),
+                    "error": {
+                        "code": code_pb2.Code.Value("DEADLINE_EXCEEDED"),
+                        "message": "This took longer than you said it should.",
+                    },
+                }
+            )
+            await future.result(timeout=600)
+
+    @pytest.mark.asyncio
+    async def test_lro_async_no_result(async_echo):
+        # As per the link below, `Some services might not provide a result`.
+        # Neither `error` or `response`.
+        # See https://github.com/googleapis/googleapis/blob/a6c9ed2d33105cb3dc9a0867a0a5d761b049b932/google/longrunning/operations.proto#L141
+        future = await async_echo.wait(
+            {
+                "end_time": datetime.now(tz=timezone.utc) + timedelta(seconds=1),
+            }
+        )
+        response = await future.result()
+        assert response is None
