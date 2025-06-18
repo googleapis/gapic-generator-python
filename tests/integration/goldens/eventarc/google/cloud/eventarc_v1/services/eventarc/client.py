b00cccf7072f88f89930375c8a0bc1eaed552f92
Anthonios Partheniou <partheniou@google.com>
2025-06-18 11:12:15 +0000

    fix: address mypy failures

--- tests/integration/goldens/eventarc/google/cloud/eventarc_v1/services/eventarc/client.py
+++ tests/integration/goldens/eventarc/google/cloud/eventarc_v1/services/eventarc/client.py
@@ -3066,10 +3066,11 @@ class EventarcClient(metaclass=EventarcClientMeta):
 
         # Certain fields should be provided within the metadata header;
         # add these here.
-        metadata = tuple(metadata) + (
-            gapic_v1.routing_header.to_grpc_metadata(
-                (("name", request.name),)),
-        )
+        if request is not None:
+            metadata = tuple(metadata) + (
+                gapic_v1.routing_header.to_grpc_metadata(
+                    (("name", request.name),)),
+            )
 
         # Validate the universe domain.
         self._validate_universe_domain()
--- tests/integration/goldens/redis/google/cloud/redis_v1/services/cloud_redis/client.py
+++ tests/integration/goldens/redis/google/cloud/redis_v1/services/cloud_redis/client.py
@@ -2245,10 +2245,11 @@ class CloudRedisClient(metaclass=CloudRedisClientMeta):
 
         # Certain fields should be provided within the metadata header;
         # add these here.
-        metadata = tuple(metadata) + (
-            gapic_v1.routing_header.to_grpc_metadata(
-                (("name", request.name),)),
-        )
+        if request is not None:
+            metadata = tuple(metadata) + (
+                gapic_v1.routing_header.to_grpc_metadata(
+                    (("name", request.name),)),
+            )
 
         # Validate the universe domain.
         self._validate_universe_domain()
