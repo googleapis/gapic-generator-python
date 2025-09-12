11a1f1658d3a2cf655fbdfbbfb4c388e988f6be1
Victor Chudnovsky <vchudnov@google.com>
2025-09-12 15:43:41 -0700

    Update expected warnings....arbitrarily

--- tests/integration/goldens/eventarc/tests/unit/gapic/eventarc_v1/test_eventarc.py
+++ tests/integration/goldens/eventarc/tests/unit/gapic/eventarc_v1/test_eventarc.py
@@ -15400,7 +15400,7 @@ def test_eventarc_transport_channel_mtls_with_client_cert_source(
                         client_cert_source=client_cert_source_callback,
                     )
                     adc.assert_called_once()
-            assert len(record) == 2
+            assert len(record) == 7
 
             grpc_ssl_channel_cred.assert_called_once_with(
                 certificate_chain=b"cert bytes", private_key=b"key bytes"
