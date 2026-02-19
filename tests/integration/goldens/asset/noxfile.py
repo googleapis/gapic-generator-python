7063041268f61f91c5c476e8f73e304f52b4e6ee
Anthonios Partheniou <partheniou@google.com>
2026-02-19 18:21:22 +0000

    also update proto-plus

--- tests/integration/goldens/asset/noxfile.py
+++ tests/integration/goldens/asset/noxfile.py
@@ -606,7 +606,7 @@ def core_deps_from_source(session, protobuf_implementation):
         "google-api-core @ git+https://github.com/googleapis/google-cloud-python#egg=google-api-core&subdirectory=packages/google-api-core",
         "google-auth @ git+https://github.com/googleapis/google-auth-library-python.git",
         "grpc-google-iam-v1 @ git+https://github.com/googleapis/google-cloud-python#egg=grpc-google-iam-v1&subdirectory=packages/grpc-google-iam-v1",
-        "proto-plus @ git+https://github.com/googleapis/proto-plus-python.git",
+        "proto-plus @ git+https://github.com/googleapis/google-cloud-python#egg=proto-plus&subdirectory=packages/proto-plus",
     ]
 
     for dep in core_dependencies_from_source:
